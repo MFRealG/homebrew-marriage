e01750c7c4773c2fa0f3caa9d1e76dff96d7c885
Zigfrid <98342238+MFRealG@users.noreply.github.com>
2025-05-17 19:34:16 +0200

    Update marriage.rb

--- Casks/marriage.rb
+++ Casks/marriage.rb
@@ -1,6 +1,6 @@
 cask "marriage" do
   version "1.0.0.1"
-  sha256 "ВАШ_ХЕШ" # Замените на актуальный SHA256 хеш файла
+  sha256 "495f9878d5c9d3017cca31bacbaf16b015778757381fef6def947ae578a280ce"
 
   url "https://github.com/MFRealG/dos-marriage/releases/download/v#{version}/MARRIAGE.app.zip",
       verified: "github.com/MFRealG/dos-marriage/"
